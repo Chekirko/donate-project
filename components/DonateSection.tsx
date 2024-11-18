0edaa2369e5b2fcd081ba5a83c096d691a1c7b8d
chekirko <victorche59@gmail.com>
2024-11-18 22:01:43 +0200

    fix styles

--- components/DonateSection.tsx
+++ components/DonateSection.tsx
@@ -3,15 +3,15 @@ import Donate from "./Donate";
 
 const DonateSection = () => {
   return (
-    <div className="container px-4 justify-center mx-auto">
+    <div className="flex flex-col mb-12 px-4 justify-center mx-auto">
       <h1
         id="donate"
-        className="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white"
+        className="mb-8 text-3xl text-center font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white"
       >
         Пожертвувати
       </h1>
       <div className="flex justify-center">
-        <div className="flex w-full justify-between gap-6 flex-col md:flex-row ">
+        <div className="flex justify-around gap-6 flex-col md:flex-row ">
           <Donate
             title="Пожертвувати на капеланське служіння"
             cardNumber="4790 7299 3036 7453"
